c9521d58269634a0859d6fa74bb07acef712fc36
Mahesh Singla <mahesh.singla@rwit.io>
2021-08-19 17:07:33 +0530

    add the code

--- assets/js/automation.js
+++ assets/js/automation.js
@@ -6,7 +6,7 @@ const myAutomation = document.getElementById('myAutomation');
 const ourDetails = document.getElementById('ourDetails');
 
 const sideBarWrapper = document.getElementById('sidebar-wrapper');
-const settingTabs =  document.getElementsByClassName('setting-tabs');
+const settingTabs = document.getElementsByClassName('setting-tabs');
 const asideAutomation = document.getElementById('aside-automation');
 const createNewBtn = document.getElementById('create-new');
 const automationUlElement = document.getElementById('created-ul');
@@ -31,7 +31,7 @@ const delParentBtns = document.querySelectorAll('.delete-its-parent');
 const addCriteria = (wrapperElement) => {
 	console.log('addCriteria');
 	wrapperElement.innerHTML +=
-	`
+		`
 	<div class="input-group">
 	<input type="text" name="" class="nested-input" value="Registration" class="">
 
@@ -103,7 +103,7 @@ const deleteAll = (wrapperElement) => {
 
 //  add criteria button 
 document.querySelectorAll('.add-criteria').forEach(addBtn => {
-	
+
 	addBtn.addEventListener('click', (e) => {
 
 		e.preventDefault();
@@ -122,7 +122,7 @@ document.querySelectorAll('.delete-all').forEach(deleteBtn => {
 		e.preventDefault();
 		deleteAll(inputGroupWrapper);
 
-		})
+	})
 })
 
 
@@ -157,38 +157,41 @@ delParentBtns.forEach((delParentBtn) => {
 /* ----------------------------------------------------------------
 SMS TEMPLATE POPUP CODES START HERE
 -------------------------------------------------------------------*/
-const smsChangeBtns = document.querySelectorAll('.sms-change-btn');
-const smsTemplateWrapper = document.getElementById('sms-template-wrapper');
-const smsTemplateClosedBtns = document.querySelectorAll('.sms-template-btns button');
+const smsFun = () => {
+	const smsChangeBtns = document.querySelectorAll('.sms-change-btn');
+	const smsTemplateWrapper = document.getElementById('sms-template-wrapper');
+	const smsTemplateClosedBtns = document.querySelectorAll('.sms-template-btns button');
 
 	// sms template closing
-const closedBtn_4_sms_template = () => {
+	const closedBtn_4_sms_template = () => {
 
-	smsTemplateClosedBtns.forEach(smsTemplateClosedBtn => {
+		smsTemplateClosedBtns.forEach(smsTemplateClosedBtn => {
 
-		smsTemplateClosedBtn.addEventListener('click', e => {
-			
-			smsTemplateWrapper.classList.add('d-none');
+			smsTemplateClosedBtn.addEventListener('click', e => {
+
+				smsTemplateWrapper.classList.add('d-none');
+
+			});
+
+		})
+
+	}
+
+	// sms template change btn opening the popup 
+	smsChangeBtns.forEach(smsChangeBtn => {
+		smsChangeBtn.addEventListener('click', e => {
+
+			smsTemplateWrapper.classList.remove('d-none');
+
+			// calling the closed button function
+			closedBtn_4_sms_template();
 
 		});
 
-	})
-	
+	});
 }
 
-// sms template change btn opening the popup 
-smsChangeBtns.forEach( smsChangeBtn => {
-	smsChangeBtn.addEventListener('click', e => {
-		
-		smsTemplateWrapper.classList.remove('d-none');
-
-		// calling the closed button function
-		closedBtn_4_sms_template();
-
-	});
-
-})
-
+smsFun();
 
 /* ----------------------------------------------------------------
 SMS TEMPLATE POPUP CODES ENDS HERE
@@ -199,69 +202,66 @@ SMS TEMPLATE POPUP CODES ENDS HERE
 
 
 
-
-
-
-
-
-
-
 /*---------------------------
 DRAGGABLE STARTS HERE
 ------------------------------*/
 
-// div which have to be grabbed and drag 
-const grabbedDiv = document.getElementById('grab-component');
+const dragFun = () => {
+	// div which have to be grabbed and drag 
+	const grabbedDiv = document.getElementById('grab-component');
 
-grabbedDiv.scrollTop = 100;
-grabbedDiv.scrollLeft = 150;
+	grabbedDiv.scrollTop = 100;
+	grabbedDiv.scrollLeft = 150;
 
-const mouseMoveHandler = function (e) {
-	// console.log('mousemove');
-	// How far the mouse has been moved
-	const dx = e.clientX - pos.x;
-	const dy = e.clientY - pos.y;
+	const mouseMoveHandler = function (e) {
+		// console.log('mousemove');
+		// How far the mouse has been moved
+		const dx = e.clientX - pos.x;
+		const dy = e.clientY - pos.y;
 
-	// Scroll the element
-	grabbedDiv.scrollTop = pos.top - dy;
-	grabbedDiv.scrollLeft = pos.left - dx;
+		// Scroll the element
+		grabbedDiv.scrollTop = pos.top - dy;
+		grabbedDiv.scrollLeft = pos.left - dx;
 
-};
-	
-const mouseUpHandler = function () {
-	// console.log('mouseup');
-	grabbedDiv.style.cursor = 'grab';
-	grabbedDiv.style.removeProperty('user-select');
-	grabbedDiv.removeEventListener('mousemove', mouseMoveHandler);
-	
-};
+	};
+
+	const mouseUpHandler = function () {
+		// console.log('mouseup');
+		grabbedDiv.style.cursor = 'grab';
+		grabbedDiv.style.removeProperty('user-select');
+		grabbedDiv.removeEventListener('mousemove', mouseMoveHandler);
+
+	};
 
 
-let pos = { top: 0, left: 0, x: 0, y: 0 };
+	let pos = { top: 0, left: 0, x: 0, y: 0 };
 
-const mouseDownHandler = function (e) {
-	// console.log('mousedown');
-    pos = {
-        // The current scroll 
-        left: grabbedDiv.scrollLeft,
-        top: grabbedDiv.scrollTop,
-        // Get the current mouse position
-        x: e.clientX,
-        y: e.clientY,
-    };
+	const mouseDownHandler = function (e) {
+		// console.log('mousedown');
+		pos = {
+			// The current scroll 
+			left: grabbedDiv.scrollLeft,
+			top: grabbedDiv.scrollTop,
+			// Get the current mouse position
+			x: e.clientX,
+			y: e.clientY,
+		};
 
-	// Change the cursor and prevent user from selecting the text
-	grabbedDiv.style.cursor = 'grabbing';
-	grabbedDiv.style.userSelect = 'none';
-	
-	
-  grabbedDiv.addEventListener('mousemove', mouseMoveHandler);
-	grabbedDiv.addEventListener('mouseup', mouseUpHandler);
+		// Change the cursor and prevent user from selecting the text
+		grabbedDiv.style.cursor = 'grabbing';
+		grabbedDiv.style.userSelect = 'none';
 
-};
 
-grabbedDiv.addEventListener('mousedown', mouseDownHandler);
+		grabbedDiv.addEventListener('mousemove', mouseMoveHandler);
+		grabbedDiv.addEventListener('mouseup', mouseUpHandler);
 
+	};
+
+	grabbedDiv.addEventListener('mousedown', mouseDownHandler);
+
+}
+
+dragFun();
 /*---------------------------
 DRAGGABLE ENDS HERE
 ------------------------------*/
@@ -273,19 +273,24 @@ DRAGGABLE ENDS HERE
 TOGGLING SWITCH STARTS HERE
 ------------------------------*/
 
-const activeBtn = document.getElementById('activeBtn');
-const activeBtnLine = document.getElementById('activeBtnLine');
-const activeBtnCircle = document.getElementById('activeBtnCircle');
-const activeBtnText = document.getElementById('activeBtnText');
+const toggleFun = () => {
 
-activeBtn.addEventListener('click', (e) => {
-	activeBtnLine.classList.toggle('line2');
-	activeBtnCircle.classList.toggle('circle2');
+	const activeBtn = document.getElementById('activeBtn');
+	const activeBtnLine = document.getElementById('activeBtnLine');
+	const activeBtnCircle = document.getElementById('activeBtnCircle');
+	const activeBtnText = document.getElementById('activeBtnText');
 
-	
-	if (activeBtnLine.className.search('line2') !== -1) activeBtnText.innerHTML = "Inactive"
-	else activeBtnText.innerHTML = 'Active';
-});
+	activeBtn.addEventListener('click', (e) => {
+		activeBtnLine.classList.toggle('line2');
+		activeBtnCircle.classList.toggle('circle2');
+
+
+		if (activeBtnLine.className.search('line2') !== -1) activeBtnText.innerHTML = "Inactive"
+		else activeBtnText.innerHTML = 'Active';
+	});
+}
+
+toggleFun();
 /*---------------------------
 TOGGLING SWITCH ENDS HERE
 ------------------------------*/
@@ -298,29 +303,30 @@ TOGGLING SWITCH ENDS HERE
 	ASIDE BAR OF AUTOMATION SECTION STARTS HERE
 -------------------------------------------------*/
 
-// ADDING THE EVENT LISTENERS TO ALL CREATED LI ELEMENTS IN AUTOMATION ASIDE BAR
-const attachingEvent = () => {
-const liElements = document.querySelectorAll('.created-li-element');
+const asideAutomationFun = () => {
+	// ADDING THE EVENT LISTENERS TO ALL CREATED LI ELEMENTS IN AUTOMATION ASIDE BAR
+	const attachingEvent = () => {
+		const liElements = document.querySelectorAll('.created-li-element');
 
-	if (liElements.length > 0) {
-		liElements.forEach((liElement) => {
-			liElement.addEventListener('click', (e) => {
+		if (liElements.length > 0) {
+			liElements.forEach((liElement) => {
+				liElement.addEventListener('click', (e) => {
 
-				// RENDERING COMPONENTS BY CLICKING ON RENDERED LIST ELEMENTS
-				console.log(e.target.id);
+					// RENDERING COMPONENTS BY CLICKING ON RENDERED LIST ELEMENTS
+					console.log(e.target.id);
 
+				})
 			})
-		})
+		}
+
 	}
 
-}
 
 
+	// RENDERING LI ELEMENTS BY CLICKING ON CREATE NEW
+	const renderingElement = (searchedValue) => {
 
-// RENDERING LI ELEMENTS BY CLICKING ON CREATE NEW
-const renderingElement = (searchedValue) =>{
-	
-	let li = `
+		let li = `
 	<li class="created-li-element" data-id='${searchedValue}'>
     	<div class="automation-text">
         <img src="./assets/images/auomation-black.svg" class="img-fluid search-icon" alt="">
@@ -331,72 +337,74 @@ const renderingElement = (searchedValue) =>{
     </button>
   </li>
 							 `;
-	automationUlElement.innerHTML += li;
+		automationUlElement.innerHTML += li;
 
-	// calling the attach event funciton
-	// attachingEvent();
+		// calling the attach event funciton
+		// attachingEvent();
 
-}	
+	}
 
 
-// TRASH BTN FOR REMOVING ELEMENT
-const deleteElement = ()=>{
-	const trashBtns = document.querySelectorAll('#trashBtn');
+	// TRASH BTN FOR REMOVING ELEMENT
+	const deleteElement = () => {
+		const trashBtns = document.querySelectorAll('#trashBtn');
 
-	trashBtns.forEach(trashBtn => {
-		
-		trashBtn.addEventListener('click', (e) => {
-			// e.stopPropagation();
-			e.target.parentElement.remove();
-		})
+		trashBtns.forEach(trashBtn => {
+
+			trashBtn.addEventListener('click', (e) => {
+				// e.stopPropagation();
+				e.target.parentElement.remove();
+			})
+
+		});
+
+	}
+
+
+
+	// ONCLICK EVENTS FIRE ON AUTOMATION BTN 
+	automation.addEventListener('click', (e) => {
+		editStoreFormId.classList.add('d-none');
+		asideAutomation.classList.remove('aside-hidden');
+		myAutomation.classList.add('active')
+		ourDetails.classList.remove('active');
+	});
+
+
+	ourDetails.addEventListener('click', (e) => {
+		editStoreFormId.classList.remove('d-none');
+		ourDetails.classList.add('active');
+		myAutomation.classList.remove('active');
+	})
+
+
+
+	// ONCLICK EVENT FIRES ON CREATE NEW MY AUTOMATION 
+	createNewBtn.addEventListener('click', (e) => {
+		e.preventDefault();
+		if (e.target.parentElement.search.value !== '') {
+			renderingElement(e.target.parentElement.search.value);
+		}
+		e.target.parentElement.search.value = '';
 
 	});
 
+
+	if (automationUlElement.childElementCount > 0) {
+		deleteElement();
+	};
+
+
+
+	// CLOSING THE AUTOMATION MENU WHEN CLICKING ON THIS BUTTON 
+	closeBtnAutomation__aside.addEventListener('click', (e) => {
+		form_ul_automation.classList.toggle('d-none');
+		asideAutomation.classList.toggle('w-auto');
+	})
+
 }
 
-
-
-// ONCLICK EVENTS FIRE ON AUTOMATION BTN 
-automation.addEventListener('click', (e)=>{
-	editStoreFormId.classList.add('d-none');
-	asideAutomation.classList.remove('aside-hidden');
-	myAutomation.classList.add('active')
-	ourDetails.classList.remove('active');
-});
-
-
-ourDetails.addEventListener('click', (e)=>{
-	editStoreFormId.classList.remove('d-none');
-	ourDetails.classList.add('active');
-	myAutomation.classList.remove('active');
-})
-
-
-
-// ONCLICK EVENT FIRES ON CREATE NEW MY AUTOMATION 
-createNewBtn.addEventListener('click', (e) =>{
-	e.preventDefault();
-	if(e.target.parentElement.search.value !== ''){
-		renderingElement(e.target.parentElement.search.value);
-	}
-	e.target.parentElement.search.value = '';
-
-});
-
-
-if (automationUlElement.childElementCount > 0) {
-	deleteElement();
-};
-
-
-
-// CLOSING THE AUTOMATION MENU WHEN CLICKING ON THIS BUTTON 
-closeBtnAutomation__aside.addEventListener('click', (e)=>{
-	form_ul_automation.classList.toggle('d-none');
-	asideAutomation.classList.toggle('w-auto');
-})
-
-
+asideAutomationFun();
 
 /* --------------------------------------------
 	ASIDE BAR OF AUTOMATION SECTION STARTS HERE
