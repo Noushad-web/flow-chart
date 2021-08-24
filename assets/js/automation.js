a09cc6defab4d19eefad8a93d9f9739294af9f02
Mahesh Singla <mahesh.singla@rwit.io>
2021-08-24 14:49:15 +0530

    add the desing

--- assets/js/automation.js
+++ assets/js/automation.js
@@ -29,17 +29,17 @@ const asideAutomationFun = () => {
 	}
 
 	// ADDING THE EVENT LISTENERS TO ALL CREATED LI ELEMENTS IN AUTOMATION ASIDE BAR
-	const attachingEvent = () => {
-		const textElements = document.querySelectorAll('.created-li-element > .automation-text');		
-		if (textElements.length > 0) {
-			textElements.forEach((textElement, elementNumber) => {
-				textElement.addEventListener('click', () => {					
-					togglingComponents();
-				})
-			})
-		}
-	}
-	attachingEvent();
+	// const attachingEvent = () => {
+	// 	const textElements = document.querySelectorAll('.created-li-element > .automation-text');
+	// 	if (textElements.length > 0) {
+	// 		textElements.forEach((textElement, elementNumber) => {
+	// 			textElement.addEventListener('click', () => {
+	// 				togglingComponents();
+	// 			})
+	// 		})
+	// 	}
+	// }
+	// attachingEvent();
 
 	// TRASH BTN FOR REMOVING ELEMENT
 	const deleteElement = () => {
@@ -54,10 +54,10 @@ const asideAutomationFun = () => {
 	// RENDERING LI ELEMENTS BY CLICKING ON CREATE NEW
 	const renderingElement = (searchedValue) => {
 		counterArray.push(counterArray.length);
-		const li = 
-		`
+		const li =
+			`
 		<li class="created-li-element" >
-		<div class="automation-text" data-id='${counterArray[counterArray.length-1]}'>
+		<div class="automation-text" data-id='${counterArray[counterArray.length - 1]}'>
 		<img src="./assets/images/auomation-black.svg" class="img-fluid search-icon" alt="">
 		<span>Gold Message</span>
 		</div>	
@@ -66,14 +66,14 @@ const asideAutomationFun = () => {
 		</button>
 		</li>
 		`;
-		automationUlElement.innerHTML += li; 
+		automationUlElement.innerHTML += li;
 		deleteElement();
-		attachingEvent();
+		// attachingEvent();
 	}
-	
+
 	// ONCLICK EVENTS FIRE ON AUTOMATION BTN 
 	automationNavBtn.addEventListener('click', (e) => {
-		
+
 		const automateSection = document.getElementById('automation-section');
 		editStoreFormId.classList.add('d-none');
 		asideAutomation.classList.remove('aside-hidden');
@@ -82,7 +82,7 @@ const asideAutomationFun = () => {
 		automateSection.classList.toggle('automation-section-height');
 		emptyBlog.classList.remove('d-none');
 		grabbedDiv.classList.add('d-none');
-		
+
 	});
 
 	ourDetails.addEventListener('click', (e) => {
@@ -119,24 +119,24 @@ asideAutomationFun();
 
 /*------------- AUTOMATION FORMS ALL CONDITIONS ---------------*/
 const formCondition = () => {
-	
+
 	// input variable and input group variables
 	const nestedInputs = document.querySelectorAll('.nested-input');
 	const inputUl = document.getElementById('input-ul');
 	const delParentBtns = document.querySelectorAll('.delete-its-parent');
-	
+
 	// functions to be used again and again
 	const inputFocusBlur = (nestedInputs) => {
 		nestedInputs.forEach(nestedInput => {
 
 			// elements to show when user focus on input 
-			nestedInput.onfocus = e => {		
+			nestedInput.onfocus = e => {
 				inputUl.classList.remove('d-none');
-				inputUl.style.top = e.target.getClientRects()[0].top + 30 +'px';
+				inputUl.style.top = e.target.getClientRects()[0].top + 30 + 'px';
 				inputUl.style.left = e.target.getClientRects()[0].left + 'px';
 			}
 			// elements to hidden when user get focus on somewhere else
-			nestedInput.onblur = () => {					
+			nestedInput.onblur = () => {
 				inputUl.classList.add('d-none');
 			}
 		})
@@ -146,11 +146,11 @@ const formCondition = () => {
 	// cross icon to delete only its parent element 
 	const deleteSelectedItem = (selectedItem) => {
 		selectedItem.remove();
-	}	
+	}
 
 	// add criteria function 
 	const addCriteria = (wrapperElement, template) => {
-		wrapperElement.appendChild(template);		
+		wrapperElement.appendChild(template);
 		document.querySelectorAll('.delete-its-parent').forEach((eachBtn) => {
 
 			eachBtn.addEventListener('click', (e) => {
@@ -164,7 +164,7 @@ const formCondition = () => {
 	}
 
 	// deleteAll
-	const deleteAll = (wrapperElement) => {	
+	const deleteAll = (wrapperElement) => {
 		wrapperElement.innerHTML = "";
 	}
 
@@ -177,9 +177,9 @@ const formCondition = () => {
 
 			if (domDrilling.search('input-group-wrapper') !== -1) {
 				addCriteria(targetElement, actionTemplates.content.cloneNode(true));
-			}else if (domDrilling.search('input-wrapper-condition') !== -1) {	
+			} else if (domDrilling.search('input-wrapper-condition') !== -1) {
 				addCriteria(targetElement, conditionTemplates.content.cloneNode(true));
-			}else {
+			} else {
 				console.error('please let the input-group-wrapper div only after the btns div to make it work right.');
 			}
 		});
@@ -188,17 +188,17 @@ const formCondition = () => {
 	// delete all button 
 	document.querySelectorAll('.delete-all').forEach(deleteBtn => {
 		deleteBtn.addEventListener('click', (e) => {
-			e.preventDefault();			
+			e.preventDefault();
 			const domDrilling = e.target.parentElement.nextElementSibling.className;
 			const targetElement = e.target.parentElement.nextElementSibling;
 
-			if (domDrilling.search('input-group-wrapper') !== -1) {	
+			if (domDrilling.search('input-group-wrapper') !== -1) {
 				deleteAll(targetElement);
-			}else if (domDrilling.search('input-wrapper-condition') !== -1 ) {
+			} else if (domDrilling.search('input-wrapper-condition') !== -1) {
 				deleteAll(targetElement);
-			}else {
+			} else {
 				console.error('please let the input-group-wrapper div only after the btns div to make it work right.');
-			}		
+			}
 		})
 	})
 
@@ -217,7 +217,7 @@ const smsFun = () => {
 	const smsChangeBtns = document.querySelectorAll('.sms-change-btn');
 	const smsTemplateWrapper = document.getElementById('sms-template-wrapper');
 	const smsTemplateClosedBtns = document.querySelectorAll('.sms-template-btns button');
-	
+
 	// sms template closing
 	const closedBtn_4_sms_template = () => {
 		smsTemplateClosedBtns.forEach(smsTemplateClosedBtn => {
@@ -238,63 +238,6 @@ const smsFun = () => {
 smsFun();
 
 
-/*--------------- DRAGGABLE STARTS HERE ----------------*/
-const dragFun = () => {
-	// div which have to be grabbed and drag 
-	grabbedDiv.scrollTop = 0;
-	grabbedDiv.scrollLeft = 0;
-
-	const mouseMoveHandler = (e) => {
-		console.log('mousemove');
-		grabbedDiv.style.cursor = 'grabbing';
-		// How far the mouse has been moved
-		const dx = e.clientX - pos.x;
-		const dy = e.clientY - pos.y;
-		// Scroll the element
-		grabbedDiv.scrollTop = pos.top - dy;
-		grabbedDiv.scrollLeft = pos.left - dx;
-	};
-
-	const keydownHandler = (e) => {		
-			if (e.code === 'Space') {
-				console.log('keydown');
-				grabbedDiv.addEventListener('mousedown', mouseDownHandler);
-			}
-	};
-
-	let pos = { top: 0, left: 0, x: 0, y: 0 };
-	const mouseDownHandler = (e) => {		
-		pos = {
-			// The current scroll 
-			left: grabbedDiv.scrollLeft,
-			top: grabbedDiv.scrollTop,
-			// Get the current mouse position
-			x: e.clientX,
-			y: e.clientY,
-		};
-
-		// Change the cursor and prevent user from selecting the text
-		grabbedDiv.style.cursor = 'grab';
-		grabbedDiv.style.userSelect = 'none';
-		grabbedDiv.addEventListener('mousemove', mouseMoveHandler);
-	};
-
-	document.addEventListener('keydown', keydownHandler);
-
-	document.addEventListener('keyup', (e) => {
-		if (e.code === 'Space') {
-			console.log('keyup');
-			grabbedDiv.style.cursor = 'auto';
-			grabbedDiv.style.removeProperty('user-select');
-			grabbedDiv.removeEventListener('mousemove', mouseMoveHandler);
-			document.removeEventListener('keyup')
-			// grabbedDiv.addEventListener('mouseup', mouseUpHandler);			
-		}
-	})
-}
-dragFun();
-
-
 /*---------------- TOGGLING SWITCH STARTS HERE ---------------*/
 const toggleFun = () => {
 
@@ -305,7 +248,7 @@ const toggleFun = () => {
 
 	activeBtn.addEventListener('click', (e) => {
 		activeBtnLine.classList.toggle('line2');
-		activeBtnCircle.classList.toggle('circle2');		
+		activeBtnCircle.classList.toggle('circle2');
 		if (activeBtnLine.className.search('line2') !== -1) activeBtnText.innerHTML = "Inactive"
 		else activeBtnText.innerHTML = 'Active';
 	});
@@ -324,7 +267,7 @@ const flowFun = () => {
 	const selectActionElementLeft = document.getElementById('action');
 	const selectActionElementRight = document.getElementById('action-right');
 
-	selectActionElementLeft.addEventListener('change', (e) => {	
+	selectActionElementLeft.addEventListener('change', (e) => {
 		if (e.target.value === 'no-action') {
 			allLeftElem.forEach((element) => {
 				element.style.visibility = 'hidden';
@@ -350,25 +293,25 @@ const flowFun = () => {
 		}
 	})
 
-	
-	sendSmsLeft.addEventListener('click', () => {	
+
+	sendSmsLeft.addEventListener('click', () => {
 		allLeftElem.forEach((element) => {
 			element.style.visibility = 'visible';
 		})
 	})
-	
+
 	noActionLeft.addEventListener('click', () => {
 		allLeftElem.forEach((element) => {
 			element.style.visibility = 'hidden';
 		})
 	})
-	
+
 	sendSmsRight.addEventListener('click', () => {
 		allRightElem.forEach((element) => {
 			element.style.visibility = 'visible';
 		})
 	});
-	
+
 	noActionRight.addEventListener('click', () => {
 		allRightElem.forEach((element) => {
 			element.style.visibility = 'hidden';
@@ -377,4 +320,77 @@ const flowFun = () => {
 }
 flowFun();
 
+
+/* _______________ DRAG THE DIV BY GRABBING ________________*/
+const dragDivFun = () => {
+	
+	let pos = { top: 0, left: 0, x: 0, y: 0 };
+	// div which have to be grabbed and drag 
+	grabbedDiv.scrollTop = 0;
+	grabbedDiv.scrollLeft = 0;
+
+	const checkEvents = (key, mouse) => {
+		let eventKey = key;
+		let eventMouse = mouse;
+		if (eventKey && eventMouse) {
+			console.log(eventKey, eventMouse);
+			grabbedDiv.addEventListener('mousemove', mousemove); // Phase 3 mousemove
+		} else {
+			grabbedDiv.removeEventListener('mousemove', mousemove);
+		}
+	}
+
+	const mouseup = (e) => {
+		checkEvents(true, false);
+		grabbedDiv.style.cursor = 'auto';
+		grabbedDiv.style.removeProperty('user-select');
+	}
+
+	const mousemove = (e)=> {
+		console.log('mousemoving');
+		grabbedDiv.style.cursor = 'grabbing';
+		// How far the mouse has been moved
+		const dx = e.clientX - pos.x;
+		const dy = e.clientY - pos.y;
+		// Scroll the element
+		grabbedDiv.scrollTop = pos.top - dy;
+		grabbedDiv.scrollLeft = pos.left - dx;
+	}
+
+	const mousedown = (e) => {
+		pos = {
+			// The current scroll 
+			left: grabbedDiv.scrollLeft,
+			top: grabbedDiv.scrollTop,
+			// Get the current mouse position
+			x: e.clientX,
+			y: e.clientY,
+		};
+		checkEvents(true, true);
+		grabbedDiv.addEventListener('mouseup', mouseup); // Phase 4 mouseup
+	};
+
+	document.addEventListener("keyup", (e) => { // phase 5 keyup
+		if (e.code === 'Space') {
+			console.log("keyup");
+			grabbedDiv.removeEventListener('mousedown', mousedown);
+			grabbedDiv.removeEventListener('mousemove', mousemove);
+			grabbedDiv.style.cursor = 'auto';
+			grabbedDiv.style.removeProperty('user-select');
+		}		
+	});
+
+	const keydown = (e)=> {
+		if (e.code === 'Space') {
+			// Change the cursor and prevent user from selecting the text
+			grabbedDiv.style.cursor = 'grab';
+			grabbedDiv.style.userSelect = 'none';
+			grabbedDiv.addEventListener('mousedown', mousedown);  // Phase 2 mousedown			
+		}
+	};
+
+	document.addEventListener("keydown", keydown); // Phase 1 keydown
+
+}
+dragDivFun();
 
