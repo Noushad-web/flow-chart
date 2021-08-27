b02d2c33727c251825f702adae220dfc2b5944b7
Mahesh Singla <mahesh.singla@rwit.io>
2021-08-27 18:31:18 +0530

    add the changes'

--- assets/js/automation.js
+++ assets/js/automation.js
@@ -31,19 +31,6 @@ const asideAutomationFun = () => {
 
 	}
 
-	// ADDING THE EVENT LISTENERS TO ALL CREATED LI ELEMENTS IN AUTOMATION ASIDE BAR
-	// const attachingEvent = () => {
-	// 	const textElements = document.querySelectorAll('.created-li-element > .automation-text');
-	// 	if (textElements.length > 0) {
-	// 		textElements.forEach((textElement, elementNumber) => {
-	// 			textElement.addEventListener('click', () => {
-	// 				togglingComponents();
-	// 			})
-	// 		})
-	// 	}
-	// }
-	// attachingEvent();
-
 	// TRASH BTN FOR REMOVING ELEMENT
 	const deleteElement = () => {
 		const trashBtns = document.querySelectorAll('#trashBtn');
@@ -113,7 +100,7 @@ const asideAutomationFun = () => {
 
 	// opening flow chart on clicking create me btn 
 	createNewBtnFlowChart.addEventListener('click', () => {
-		togglingComponents();
+		togglingComponents();		
 	})
 
 }
@@ -267,7 +254,7 @@ const flowFun = () => {
 	const sendSmsLeft = document.getElementById('sms-left');
 	const noActionRight = document.getElementById('no-action-right');
 	const sendSmsRight = document.getElementById('sms-right');
-	const selectActionElementLeft = document.getElementById('action');
+	const selectActionElementLeft = document.getElementById('action-big-left');
 	const selectActionElementRight = document.getElementById('action-right');
 
 	selectActionElementLeft.addEventListener('change', (e) => {
@@ -296,7 +283,7 @@ const flowFun = () => {
 		}
 	})
 
-
+console.log(sendSmsLeft);
 	sendSmsLeft.addEventListener('click', () => {
 		allLeftElem.forEach((element) => {
 			element.style.visibility = 'visible';
@@ -332,7 +319,7 @@ const zoomFun = () => {
 	let scale = [1];
 
 	const zoomInHandler = (e) => {
-		if (scale.length-1 < 2) {
+		if (scale.length-1 < 1) {
 			scale.push((scale[scale.length-1]) * 1.5);
 			componentWrapper.style.transform = `scale(${scale[scale.length-1]})`;
 		}
@@ -341,85 +328,11 @@ const zoomFun = () => {
 		if ( scale.length-1 > 0 ) {
 			scale.pop(scale.length - 1);
 			componentWrapper.style.transform = `scale(${scale})`;
-		}		
+		}
 	}
-
 	zoomIn.addEventListener('click', zoomInHandler);
 	zoomOut.addEventListener('click', zoomOutHandler);
 }
 zoomFun();
-
-
-
-/* _______________ DRAG THE DIV BY GRABBING ________________*/
-// const dragDivFun = () => {
-	
-// 	let pos = { top: 0, left: 0, x: 0, y: 0 };
-// 	// div which have to be grabbed and drag 
-// 	grabbedDiv.scrollTop = 0;
-// 	grabbedDiv.scrollLeft = 0;
-
-// 	const checkEvents = (key, mouse) => {
-// 		let eventKey = key;
-// 		let eventMouse = mouse;
-// 		if (eventKey && eventMouse) {
-// 			console.log(eventKey, eventMouse);
-// 			grabbedDiv.addEventListener('mousemove', mousemove); // Phase 3 mousemove
-// 		} else {
-// 			grabbedDiv.removeEventListener('mousemove', mousemove);
-// 		}
-// 	}
-
-// 	const mouseup = (e) => {
-// 		checkEvents(true, false);
-// 		grabbedDiv.style.cursor = 'auto';
-// 		grabbedDiv.style.removeProperty('user-select');
-// 	}
-
-// 	const mousemove = (e)=> {
-// 		grabbedDiv.style.cursor = 'grabbing';
-// 		// How far the mouse has been moved
-// 		const dx = e.clientX - pos.x;
-// 		const dy = e.clientY - pos.y;
-// 		// Scroll the element
-// 		grabbedDiv.scrollTop = pos.top - dy;
-// 		grabbedDiv.scrollLeft = pos.left - dx;
-// 	}
-
-// 	const mousedown = (e) => {
-// 		pos = {
-// 			// The current scroll 
-// 			left: grabbedDiv.scrollLeft,
-// 			top: grabbedDiv.scrollTop,
-// 			// Get the current mouse position
-// 			x: e.clientX,
-// 			y: e.clientY,
-// 		};
-// 		checkEvents(true, true);
-// 		grabbedDiv.addEventListener('mouseup', mouseup); // Phase 4 mouseup
-// 	};
-
-// 	document.addEventListener("keyup", (e) => { // phase 5 keyup
-// 		if (e.code === 'ShiftRight' || e.code === 'ShiftLeft') {
-// 			grabbedDiv.removeEventListener('mousedown', mousedown);
-// 			grabbedDiv.removeEventListener('mousemove', mousemove);
-// 			grabbedDiv.style.cursor = 'auto';
-// 			grabbedDiv.style.removeProperty('user-select');
-// 		}		
-// 	});
-
-// 	const keydown = (e) => {
-// 		if (e.code === 'ShiftRight' || e.code === 'ShiftLeft') {
-// 			// Change the cursor and prevent user from selecting the text
-// 			grabbedDiv.style.cursor = 'grab';
-// 			grabbedDiv.style.userSelect = 'none';
-// 			grabbedDiv.addEventListener('mousedown', mousedown);  // Phase 2 mousedown			
-// 		}
-// 	};
-
-// 	document.addEventListener("keydown", keydown); // Phase 1 keydown
-
-// }
-// dragDivFun();
 
 
